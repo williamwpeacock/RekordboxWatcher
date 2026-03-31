15c1d5f4a6f072f22f12cd088948c42424c4d207
will <william.wpeacock@gmail.com>
2026-03-31 16:37:39 +0100

    updated config classes to reflect scaling updates

--- rekordbox_watcher/layout.py
+++ rekordbox_watcher/layout.py
@@ -1,9 +1,10 @@
 import json
 
 from pydantic import BaseModel
-from typing import List, Optional
+from enum import Enum
+from typing import List, Optional, Dict, Type
 
-from .extraction import TextExtraction, BPMExtraction, TimeExtraction, IsMasterExtraction, IsPlayingExtraction, ModeExtraction, LayoutExtraction, VolumeExtraction, EQExtraction, IsLoadedExtraction
+from .extraction import ExtractionStrategyFactory, ExtractionStrategies, ModeExtraction, LayoutExtraction, ExtractionArea, STRATEGY_MAP, Scaler
 
 def load_from_json(json_path):
     """Imports bounding boxes from JSON."""
@@ -12,78 +13,86 @@ def load_from_json(json_path):
 
     return Config.from_json(json_obj)
 
-class EQConfig(BaseModel):
-    """Extraction strategies for EQ values.
+class AnchorSide(str, Enum):
+    LEFT = "LEFT"
+    RIGHT = "RIGHT"
 
-    Attributes:
-        high (EQExtraction): Extraction strategy for high EQ value.
-        medium (EQExtraction): Extraction strategy for medium EQ value.
-        low (EQExtraction): Extraction strategy for low EQ value.
-    """
-    high: EQExtraction
-    medium: EQExtraction
-    low: EQExtraction
+class Property(BaseModel):
+    bb: List[List[int]]
+    left_anchor: AnchorSide
+    right_anchor: AnchorSide
+    extraction_strategy: Type[ExtractionArea]
 
     @staticmethod
     def from_json(json_obj):
-        return EQConfig(
-            high=EQExtraction.from_json(json_obj["high"]),
-            medium=EQExtraction.from_json(json_obj["medium"]),
-            low=EQExtraction.from_json(json_obj["low"])
+        extraction_strategy = STRATEGY_MAP[json_obj["extraction_strategy"]]
+        return Property(
+            bb = json_obj["bb"],
+            left_anchor = json_obj["left_anchor"],
+            right_anchor = json_obj["right_anchor"],
+            extraction_strategy = extraction_strategy,
+        )
+
+class DeckProperties(BaseModel):
+    song: Property
+    is_loaded: Property
+
+    artist: Property
+
+    is_master: Property
+    time: Property
+
+    bpm: Property
+    is_playing: Property
+
+    @staticmethod
+    def from_json(json_obj):
+        return DeckProperties(
+            song = Property.from_json(json_obj["song"]),
+            is_loaded = Property.from_json(json_obj["is_loaded"]),
+            artist = Property.from_json(json_obj["artist"]),
+            is_master = Property.from_json(json_obj["is_master"]),
+            time = Property.from_json(json_obj["time"]),
+            bpm = Property.from_json(json_obj["bpm"]),
+            is_playing = Property.from_json(json_obj["is_playing"])
+        )
+
+class MixerProperties(BaseModel):
+    volume: Property
+    high: Property
+    medium: Property
+    low: Property
+
+    @staticmethod
+    def from_json(json_obj):
+        return MixerProperties(
+            volume = Property.from_json(json_obj["volume"]),
+            high = Property.from_json(json_obj["high"]),
+            medium = Property.from_json(json_obj["medium"]),
+            low = Property.from_json(json_obj["low"])
         )
 
 class DeckConfig(BaseModel):
-    """Extraction strategies for a deck.
-
-    Attributes:
-        song (TextExtraction): Extraction strategy for song name.
-        is_loaded (IsLoadedExtraction): Extraction strategy for is_loaded boolean.
-        artist (TextExtraction): Extraction strategy for artist name.
-        is_master (IsMasterExtraction): Extraction strategy for is_master boolean.
-        bpm (BPMExtraction): Extraction strategy for BPM value.
-        time (TimeExtraction): Extraction strategy for time value.
-        is_playing (IsPlayingExtraction): Extraction strategy for is_playing boolean.
-        volume (VolumeExtraction): Extraction strategy for volume value.
-        eq (EQConfig): Extraction strategies for EQ values.
-    """
-    song: TextExtraction
-    is_loaded: IsLoadedExtraction
-    artist: TextExtraction
-    is_master: IsMasterExtraction
-    bpm: BPMExtraction
-    time: TimeExtraction
-    is_playing: IsPlayingExtraction
-    volume: VolumeExtraction
-    eq: EQConfig
+    deck_properties: DeckProperties
+    mixer_properties: MixerProperties
 
     @staticmethod
     def from_json(json_obj):
         return DeckConfig(
-            song=TextExtraction.from_json(json_obj["song"]),
-            is_loaded=IsLoadedExtraction.from_json(json_obj["song"]),
-            artist=TextExtraction.from_json(json_obj["artist"]),
-            is_master=IsMasterExtraction.from_json(json_obj["is_master"]),
-            bpm=BPMExtraction.from_json(json_obj["bpm"]),
-            time=TimeExtraction.from_json(json_obj["time"]),
-            is_playing=IsPlayingExtraction.from_json(json_obj["is_playing"]),
-            volume=VolumeExtraction.from_json(json_obj["volume"]),
-            eq=EQConfig.from_json(json_obj["eq"])
+            deck_properties = DeckProperties.from_json(json_obj["deck_properties"]),
+            mixer_properties = MixerProperties.from_json(json_obj["mixer_properties"]),
         )
 
 class LayoutConfig(BaseModel):
-    """Extraction strategies for a layout.
-
-    Attributes:
-        name (str): Layout name.
-        decks (list of DeckConfig): Extraction strategies for each deck.
-    """
     name: str
-    decks: List[DeckConfig]
+    deck_configs: List[DeckConfig]
 
     @staticmethod
-    def from_json(json_obj):
-        name, deck_layouts = list(json_obj.items())[0]
-        return LayoutConfig(name=name, decks=[DeckConfig.from_json(deck) for deck in deck_layouts])
+    def from_json(name, json_obj):
+        return LayoutConfig(
+            name = name,
+            deck_configs = [DeckConfig.from_json(deck) for deck in json_obj]
+        )
 
 class Config(BaseModel):
     """Extraction strategies for a rekordbox session.
@@ -93,29 +102,38 @@ class Config(BaseModel):
         layout (LayoutExtraction): Extraction strategy for layout string.
         layout_configs (list of LayoutConfig): Extraction strategies for each layout.
     """
+    extraction_strategy_factory: ExtractionStrategyFactory
     mode: ModeExtraction
     layout: LayoutExtraction
-    layout_configs: List[LayoutConfig]
+    layout_configs: Dict[str, LayoutConfig]
 
     @staticmethod
     def from_json(json_obj):
+        scaler = Scaler(
+            screen_width = json_obj["screen_width"],
+            screen_height = json_obj["screen_height"],
+            mixer_width = json_obj["mixer_width"]
+        )
         return Config(
+            extraction_strategy_factory = ExtractionStrategyFactory(
+                scaler = scaler
+            ),
             mode=ModeExtraction.from_json(json_obj["mode"]),
             layout=LayoutExtraction.from_json(json_obj["layout"]),
-            layout_configs=[
-                LayoutConfig.from_json({name: deck_layouts}) for name, deck_layouts in json_obj["layout_configs"].items()
-            ]
+            layout_configs={
+                name: LayoutConfig.from_json(name, deck_layouts) for name, deck_layouts in json_obj["layout_configs"].items()
+            }
         )
 
-    def get_current_layout(self, image) -> Optional[LayoutConfig]:
-        """Extracts mode and layout and returns matching LayoutConfig."""
+    def get_extraction_strategies(self, image) -> Optional[ExtractionStrategies]:
+        """Extracts mode and layout and returns matching ExtractionStrategies object, or None."""
         mode_str = self.mode.extract_from_image(image)
         if mode_str != "PERFORMANCE":
             return None
 
         layout_str = self.layout.extract_from_image(image)
-        for layout_obj in self.layout_configs:
-            if layout_obj.name == layout_str:
-                return layout_obj
+        if layout_str in self.layout_configs:
+            width, _ = image.size
+            return self.extraction_strategy_factory.get_strategies(width, self.layout_configs[layout_str])
 
         return None
