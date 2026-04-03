3f4c628396d7773b5868464281c271222bf6066f
will <william.wpeacock@gmail.com>
2026-04-03 16:27:14 +0100

    refactor to add extractor WIP

--- rekordbox_watcher/extraction.py
+++ rekordbox_watcher/extraction.py
@@ -83,7 +83,7 @@ class BoundingBox(BaseModel):
                             int(min(self.top_left.x, self.bottom_right.x)) : int(max(self.top_left.x, self.bottom_right.x))]) #,
                             # "RGB")
 
-class ExtractionArea(BaseModel):
+class ExtractionStrategyBase(BaseModel):
     """Base class for extraction strategy.
 
     Attributes:
@@ -108,21 +108,21 @@ class ExtractionArea(BaseModel):
         sub_image = self.bb.extract_from_image(image)
         return self._extract_from_image(sub_image)
 
-class TextExtraction(ExtractionArea):
+class TextExtraction(ExtractionStrategyBase):
     """Strategy to extract text from image."""
     def _extract_from_image(self, image) -> str:
         new_image = get_high_contrast_image(image)
         text = pytesseract.image_to_string(new_image, lang='eng', config='--psm 7').strip()
         return text
 
-class NumberExtraction(ExtractionArea):
+class NumberExtraction(ExtractionStrategyBase):
     """Strategy to extract number from image."""
     def _extract_from_image(self, image) -> float:
         new_image = get_high_contrast_image(image)
         num = pytesseract.image_to_string(new_image, config="--psm 7 digits").strip()
         return num
 
-class ColorExtraction(ExtractionArea):
+class ColorExtraction(ExtractionStrategyBase):
     """Strategy to extract color from image."""
     def _extract_from_image(self, image) -> Tuple[int, int, int]:
         pix = np.array(image)
@@ -238,171 +238,3 @@ class EQExtraction(ColorExtraction):
 
         # ensure value is resonable
         return np.round(value, 2)
-
-# Scaling
-
-class ScaledAnchorPoints(BaseModel):
-    deck_left_pos: int
-    deck_right_pos: int
-    mixer_left_pos: int
-    mixer_right_pos: int
-    y_offset: int
-
-    def _anchor_to(self, left_pos, right_pos, property_config):
-        result = [[0, 0], [0, 0]]
-
-        left_x = property_config.bb[0][0]
-        if property_config.left_anchor == "LEFT":
-            result[0][0] = left_pos + left_x
-        elif property_config.left_anchor == "RIGHT":
-            result[0][0] = right_pos + left_x
-
-        result[0][1] = (property_config.bb[0][1] + self.y_offset)
-
-        right_x = property_config.bb[1][0]
-        if property_config.right_anchor == "LEFT":
-            result[1][0] = left_pos + right_x
-        elif property_config.right_anchor == "RIGHT":
-            result[1][0] = right_pos + right_x
-
-        result[1][1] = (property_config.bb[1][1] + self.y_offset)
-
-        return result
-
-    def anchor_to_deck(self, property_config):
-        return self._anchor_to(self.deck_left_pos, self.deck_right_pos, property_config)
-
-    def anchor_to_mixer(self, property_config):
-        return self._anchor_to(self.mixer_left_pos, self.mixer_right_pos, property_config)
-
-class Platform(str, Enum):
-    WINDOWS = "windows"
-    MAC = "mac"
-    UNKNOWN = "unknown"
-
-    @staticmethod
-    def detect(self):
-        if platform in ["win32", "win64"]:
-            return Platform.WINDOWS
-        elif platform == "darwin":
-            return Platform.MAC
-        else:
-            return Platform.UNKNOWN
-
-class Scaler(BaseModel):
-    screen_width: int
-    screen_height: int
-    mixer_width: int
-    y_offset: int
-
-    @property
-    def default_deck_width(self):
-        return (self.screen_width - self.mixer_width) / 2
-
-    @staticmethod
-    def get_y_offset(platform):
-        if platform == Platform.WINDOWS:
-            return 0
-        elif platform == Platform.MAC:
-            return -45
-        else:
-            raise ValueError(f"Unkown Y offset for platform: {platform}")
-
-    def calculate_scaling_factor(self, current_screen_width):
-        return (current_screen_width - self.mixer_width) / (self.screen_width - self.mixer_width)
-
-    def calculate_current_deck_width(self, current_screen_width):
-        scaling_factor = self.calculate_scaling_factor(current_screen_width)
-        return scaling_factor * self.default_deck_width
-
-    def get_mixer_x_pos(self, current_screen_width):
-        return self.calculate_current_deck_width(current_screen_width)
-
-    def get_deck_x_pos(self, deck_num, current_screen_width):
-        is_left_deck = (deck_num % 2 == 0)
-        if is_left_deck:
-            return 0
-
-        return self.calculate_current_deck_width(current_screen_width) + self.mixer_width
-
-    def get_scaled_anchor_points(self, deck_num, current_screen_width):
-        deck_left_pos = self.get_deck_x_pos(deck_num, current_screen_width)
-        deck_right_pos = deck_left_pos + self.calculate_current_deck_width(current_screen_width)
-        mixer_left_pos = self.get_mixer_x_pos(current_screen_width)
-        mixer_right_pos = mixer_left_pos + self.mixer_width
-
-        return ScaledAnchorPoints(
-            deck_left_pos = int(deck_left_pos),
-            deck_right_pos = int(deck_right_pos),
-            mixer_left_pos = int(mixer_left_pos),
-            mixer_right_pos = int(mixer_right_pos),
-            y_offset = self.y_offset
-        )
-
-# Extraction strategy factory and containers
-
-class DeckExtractionStrategies(BaseModel):
-    song: Optional[TextExtraction] = None
-    is_loaded: Optional[IsLoadedExtraction] = None
-    artist: Optional[TextExtraction] = None
-    is_master: Optional[IsMasterExtraction] = None
-    bpm: Optional[BPMExtraction] = None
-    time: Optional[TimeExtraction] = None
-    is_playing: Optional[IsPlayingExtraction] = None
-    volume: Optional[VolumeExtraction] = None
-    high: Optional[EQExtraction] = None
-    medium: Optional[EQExtraction] = None
-    low: Optional[EQExtraction] = None
-
-    def __init__(self, anchor_points: ScaledAnchorPoints, deck_config):
-        super().__init__()
-        deck_properties_dict = deck_config.deck_properties.__dict__
-        for property_name, property_config in deck_properties_dict.items():
-            extraction_cls = property_config.extraction_strategy
-            anchored_bb = anchor_points.anchor_to_deck(property_config)
-            obj = extraction_cls(
-                bb=BoundingBox.from_json(anchored_bb)
-            )
-            setattr(self, property_name, obj)
-
-        mixer_properties_dict = deck_config.mixer_properties.__dict__
-        for property_name, property_config in mixer_properties_dict.items():
-            extraction_cls = property_config.extraction_strategy
-            anchored_bb = anchor_points.anchor_to_mixer(property_config)
-            obj = extraction_cls(
-                bb=BoundingBox.from_json(anchored_bb)
-            )
-            setattr(self, property_name, obj)
-
-class ExtractionStrategies(BaseModel):
-    decks: List[DeckExtractionStrategies]
-
-    def __init__(self, scaler: "Scaler", screen_width: int, deck_configs):
-        super().__init__(decks=[])
-
-        for deck_num, deck_config in enumerate(deck_configs):
-            anchor_points = scaler.get_scaled_anchor_points(deck_num, screen_width)
-            self.decks.append(
-                DeckExtractionStrategies(
-                    anchor_points = anchor_points,
-                    deck_config = deck_config
-                )
-            )
-
-class ExtractionStrategyFactory(BaseModel):
-    scaler: Scaler
-    _extraction_strategies: Dict[int, Dict[str, ExtractionStrategies]]
-
-    def get_strategies(self, width, layout_config) -> ExtractionStrategies:
-        if not hasattr(self, "_extraction_strategies"):
-            self._extraction_strategies = {}
-
-        if width not in self._extraction_strategies:
-            self._extraction_strategies[width] = {}
-
-        layout_str = layout_config.name
-
-        if layout_str not in self._extraction_strategies[width]:
-            self._extraction_strategies[width][layout_str] = ExtractionStrategies(self.scaler, width, layout_config.deck_configs)
-
-        return self._extraction_strategies[width][layout_str]
